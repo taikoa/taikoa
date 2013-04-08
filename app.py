7e201751a08c88ec5b14393c6c34900f03d1c234
Javier Aguirre <contacto@javaguirre.net>
2013-04-08 19:19:55 +0200

    Adding mail and contact form

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 from flaskext.babel import Babel
+from flask.ext.mail import Mail, Message
+
 app = Flask(__name__)
 #app.config.from_pyfile('mysettings.cfg')
 babel = Babel(app)
+mail = Mail(app)
 
 
 @babel.localeselector
@@ -19,16 +22,34 @@ def index():
 def projects():
     return render_template('projects.html')
 
+
 @app.route("/about")
 def about():
     return render_template('about.html')
+
 
 @app.route("/labs")
 def labs():
     return render_template('labs.html')
 
+
 @app.route("/contact")
 def contact():
+    return render_template('contact.html')
+
+
+@app.route("/contact_form", methods=['POST'])
+def contact_form():
+    # FIXME Validation
+    subject = request.form['subject']
+    content = request.form['content']
+    client_email = request.form['email']
+    msg = Message(subject,
+                  sender=client_email,
+                  recipients=["javi@javaguirre.net"])
+    msg.body = content
+    mail.send(msg)
+
     return render_template('contact.html')
 
 if __name__ == "__main__":
