92ff0716796d9139bfbb96f0d583ef5311dccc78
Javier Aguirre <contacto@javaguirre.net>
2013-05-03 16:09:08 +0200

    Adding timeline

---
+++ static/js/experience-config.js
@@ -0,0 +1,11 @@
+var timeline_config = {
+    width:              '100%',
+    height:             '600',
+    source:             '/static/js/experience.json',
+    embed_id:           'timeline-embed',
+    start_at_end:       false,
+    hash_bookmark:      true,
+    debug:              false,
+    css:                '/static/css/timeline/timeline.css',
+    js:                 '/static/js/timeline-min.js'
+};
